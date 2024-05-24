cd4db9fb2a43a7d80327464cf9e278bf56cfa4f4
Michael Hofweller <michael.hofweller@gmail.com>
2024-05-24 12:48:28 -0700

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export default async function Index() {
                     <AuthButton />
                 </div>
             </nav>
+            <div>test</div>
 
             <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
                 <Header />
