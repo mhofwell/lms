c8462c08aeb9051058e7f02ebc34b67c021c16ef
Michael Hofweller <michael.hofweller@gmail.com>
2024-05-28 15:47:44 -0700

    front page updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default async function Index() {
         <div className="flex-1 w-full flex flex-col gap-20 items-center">
             {/* Nav */}
             <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-                    <div>Avatar/Logo</div>
+                <div className="w-full max-w-4xl font-bold flex justify-between items-center p-3 text-sm">
+                    <div>LMS</div>
                     <AuthButton />
                 </div>
             </nav>
@@ -16,10 +16,10 @@ export default async function Index() {
             <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
                 <Header />
                 <main className="flex-1 flex justify-center  gap-6">
-                    <h2 className="font-bold text-4xl mb-4">Test</h2>
+                    <h2 className="font-bold text-4xl mb-4"></h2>
                 </main>
             </div>
-            
+
             {/* Footer */}
             <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
                 <p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // darkMode: 'class', // 'media' or 'class'
   theme: {
     extend: {
       colors: {
